451e882a9a4914eb08ff7e65cdf27e33ad968de8
Devon Riley <devontriley@gmail.com>
2018-03-20 16:57:00 -0400

    added airtable api

--- wp-content/themes/known-documentation/.build/gulpfile.js
+++ wp-content/themes/known-documentation/.build/gulpfile.js
@@ -5,34 +5,29 @@ var minify = require('gulp-minify');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
+var path = './../';
 
 gulp.task('sass', function () {
-    return gulp.src('./../styles/**/*.scss')
+    return gulp.src(path+'/styles/**/*.scss')
         .pipe(sass({errLogToConsole: true}))
         .pipe(autoprefixer())
         .pipe(concat('style.css'))
-        .pipe(gulp.dest('./../'));
+        .pipe(gulp.dest(path));
 });
 
 gulp.task('scripts', function() {
     return gulp.src([
-        './bower_components/jquery/dist/jquery.min.js',
-        './node_modules/jquery-ui/ui/widget.js',
-        './node_modules/jquery-ui/ui/version.js',
-        './node_modules/jquery-ui/ui/position.js',
-        './node_modules/jquery-ui/ui/keycode.js',
-        './node_modules/jquery-ui/ui/unique-id.js',
-        './node_modules/jquery-ui/ui/widgets/autocomplete.js',
-        './node_modules/jquery-ui/ui/widgets/menu.js',
-        './node_modules/rellax/rellax.min.js',
-        './../js/src/**/*.js'
+        path+'/.build/node_modules/jquery/dist/jquery.min.js',
+        path+'/.build/node_modules/airtable/build/airtable.browser.js',
+        path+'/.build/node_modules/axios/dist/axios.min.js',
+        path+'/js/src/**/*.js'
     ])
         .pipe(concat('all.js'))
         //.pipe(minify())
-        .pipe(gulp.dest('./../js/dist/'));
+        .pipe(gulp.dest(path+'/js/dist/'));
 });
 
 gulp.task('default', function(){
-    gulp.watch('./../styles/**/*.scss', ['sass']);
-    gulp.watch('./../js/src/*.js', ['scripts'])
+    gulp.watch(path+'/styles/**/*.scss', ['sass']);
+    gulp.watch(path+'/js/src/*.js', ['scripts'])
 });
